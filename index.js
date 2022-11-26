a72c59ffc341af52523ee53883ab73da71713789
mateusbra <suehtamzarb@gmail.com>
2022-11-25 21:59:57 -0300

    remove multer and serverless functions

--- index.js
+++ index.js
@@ -4,11 +4,6 @@ const cors = require("cors");
 const firebase_app = require('firebase/app');
 const PORT = 8000;
 const {getDatabase, ref, get, update} = require('firebase/database');
-const fs = require('fs')
-const path = require('path')
-
-
-
 
 const firebaseApp = firebase_app.initializeApp ({
   apiKey: "AIzaSyDkDXEzV5L6OxDSd7CFiQwrGEBuQeg59-E",
@@ -20,117 +15,20 @@ const firebaseApp = firebase_app.initializeApp ({
   appId: "1:851470719394:web:87be0f43da852b7f8abafb"
 }, 'firebaseApp');
 
-const multer = require("multer");
-const upload = multer({ dest: 'tmp/' })
 const firebase_storage = require('firebase/storage');
-const { uploadBytes } = require("firebase/storage");
-
-
-const storage = firebase_storage.getStorage(firebaseApp);
-const storageRef = firebase_storage.ref(storage, 'images/vavazin.jpg');
-
+const { getDownloadURL } = require("firebase/storage");
 
 const db = getDatabase(firebaseApp);
-
-
+const storage = firebase_storage.getStorage(firebaseApp);
 
 app.use(cors());
 
-app.post('/uploadImage',upload.single('file'),function(req,res){
-  
-    var fileName = req.file.path;
-    var filePath = path.join(__dirname, fileName);
-
-
-        fs.readFile(filePath, function(err,data){
-
-    if (!err) {
-        uploadBytes(storageRef, data).then((snapshot) => {
-            console.log('Uploaded a blob or file!');
-          });
-
-        
-        res.write("Imagem armazenada!");
-        res.end();
-    } else {
-        console.log(err);
-    }
-});
+app.post('/getURLImage',function(req,res){
     
-})
-
-app.get('/getLumin',async function(req,res){
-    // Get a database reference to our posts
-    await get(ref(db, `luminosity`)).then((snapshot) => {
-        if (snapshot.exists()) {
-            res.send(JSON.stringify({luminosity:snapshot.val()}));
-        }
-    });
-})
-
-app.post('/setMotion',function(req,res){
-    let motion = req.query.motion  === "true";
-    update(ref(db, `/`), {
-        motion:motion,
-      });
-      res.send("status do movimento alterado!");
-})
-
-app.get('/getMotion',async function(req,res){
-    await get(ref(db, `motion`)).then((snapshot) => {
-        if (snapshot.exists()) {
-            res.send(JSON.stringify({motion:snapshot.val()}));
-        }
-    });
-})
-
-app.post('/setMode',function(req,res){
-    let mode = req.query.mode === "true";
-    update(ref(db, `/`), {
-        mode:mode,
-      });
-      res.send("modo alterado!");
-})
-
-app.get('/getMode',async function(req,res){
-    await get(ref(db, `mode`)).then((snapshot) => {
-        if (snapshot.exists()) {
-            res.send(JSON.stringify({mode:snapshot.val()}));
-        }
-    });
-})
-
-app.post('/setRgb',function(req,res){
-    let rgb = req.query.rgb.toUpperCase();
-    console.log(rgb);
-    update(ref(db, `/`), {
-        rgb:rgb,
-      });
-      res.send("rgb alterado!");
-})
-
-app.get('/getRgb',async function(req,res){
-    await get(ref(db, `rgb`)).then((snapshot) => {
-        if (snapshot.exists()) {
-            res.send(JSON.stringify({rgb:snapshot.val()}));
-        }
-    });
-})
-
-app.post('/setLight',function(req,res){
-    let light = req.query.light === "true";
-    update(ref(db, `/`), {
-        light:light,
-      });
-      res.send("status da luz alterado!");
-})
-
-app.get('/getLight',async function(req,res){
-    await get(ref(db, `light`)).then((snapshot) => {
-        if (snapshot.exists()) {
-            res.send(JSON.stringify({light:snapshot.val()}));
-        }
-    });
+    getDownloadURL(firebase_storage.ref(storage, 'images/koo.jpg')).then((url) => {
+        res.send(JSON.stringify({URL:url}))
+    })
+    
 })
 
 app.listen(PORT, () => {
